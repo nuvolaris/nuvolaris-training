65b8566be09e7d4050157856cd3bd30375bb3773
Michele Sciabarra <michele@sciabarra.com>
2022-02-11 13:23:56 +0100

    kopf demo

--- 2-kopf/pres/pres.017.sh
+++ 2-kopf/pres/pres.017.sh
@@ -6,6 +6,7 @@ cat demo1.py
 # run the operator specifying the namespace
 poetry run kopf run -n demo demo1.py
 # new terminal
+cd ../lab
 kubectl apply -f demo-obj.yaml
 kubectl -n demo get sam
 kubectl delete -f demo-obj.yaml
