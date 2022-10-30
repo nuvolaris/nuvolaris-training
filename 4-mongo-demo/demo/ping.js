ffb1360fcae0f5ce055da8bcdd4659d352d0e340
Michele Sciabarra <michele@sciabarra.com>
2022-10-30 12:00:06 +0000

    demo mongodb

--- 4-mongo-demo/demo/ping.js
+++ 4-mongo-demo/demo/ping.js
@@ -2,7 +2,7 @@ const {MongoClient} = require('mongodb');
 
 async function main(args) {
    const client = new MongoClient(args.mongodb);
-    await client.connect() 
-    return client.db().command({ping:1}) 
+   await client.connect() 
+   return client.db().command({ping:1}) 
 }
- 
+
